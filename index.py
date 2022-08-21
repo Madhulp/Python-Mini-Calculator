4df70a4551abf65ced852925389a130dce3ff464
madhul patil <madhulpatil7@gmail.com>
2022-08-21 19:00:14 +0530

    python mini calculator

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
 # python mini calculator
+
+first = int(input("Enter a Number: "))
+operator = input("Enter Operator: ")
+second = int(input("Enter a Number: "))
+if operator == "+":
+    print(first+second)
+elif operator == "-":
+    print(first-second)
+elif operator == "/":
+    print(first/second)
+elif operator == "*":
+    print(first*second)
