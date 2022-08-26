d632092c3d30824f7b302e8aaa95d52efc8f055d
madhul patil <madhulpatil7@gmail.com>
2022-08-26 14:39:00 +0530

    Python Mini Calculator

--- index.py
+++ index.py
@@ -3,6 +3,7 @@
 first = int(input("Enter a Number: "))
 operator = input("Enter Operator: ")
 second = int(input("Enter a Number: "))
+
 if operator == "+":
     print(first+second)
 elif operator == "-":
